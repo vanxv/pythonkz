0ed5077a2fbc2977d18d0bbd3a443e853440ee44
van <ironvanxv@gmail.com>
2016-12-23 23:25:54 +0800

    add ex31

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print "hello world!"
+print "hello again"
+print "i like typing thsis."
+print "this is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'."
+print 'i "said" do not touch this.'
